fe3a84dc55ae5ff4d04a1a69f8521292c8d588c9
Travis Haby <travishaby@gmail.com>
2017-07-20 22:23:10 -0600

    Add length to edge

--- lib/edge.rb
+++ lib/edge.rb
@@ -1,7 +1,8 @@
 class Edge
-  attr_reader :start, :terminus
-  def initialize(start:, terminus:)
+  attr_reader :start, :terminus, :length
+  def initialize(start:, terminus:, length:)
     @start = start
     @terminus = terminus
+    @length = length
   end
 end
--- test/edge_test.rb
+++ test/edge_test.rb
@@ -1,12 +1,13 @@
 require_relative 'test_helper.rb'
 
 describe Edge do
-  it "has a start vertex and an end vertex" do
+  it "has a start vertex, terminus vertex, and a distance" do
     vertex_a = Vertex.new('a')
     vertex_b = Vertex.new('b')
-    edge = Edge.new(start: vertex_a, terminus: vertex_b)
+    edge = Edge.new(start: vertex_a, terminus: vertex_b, length: 5)
 
     expect(edge.start).must_equal vertex_a
     expect(edge.terminus).must_equal vertex_b
+    expect(edge.length).must_equal 5
   end
 end
